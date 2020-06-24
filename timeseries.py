9d158611a60c21cee27bbf01b9f5b911cd10e664
TeddyTW <65396509+TeddyTW@users.noreply.github.com>
2020-06-24 09:42:20 +0100

    comments addressed

--- timeseries.py
+++ timeseries.py
@@ -9,7 +9,8 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 from datetime import datetime
 
-def MA(df: pd.DataFrame, detector: str, past_days: int)-> float:
+
+def MA(df: pd.DataFrame, detector: str, past_days: int) -> float:
     """Function calculates the average number of vehicles over the last past_days
 
     Args:
@@ -23,10 +24,11 @@ def MA(df: pd.DataFrame, detector: str, past_days: int)-> float:
         """
 
     one_D = df[df["detector_id"] == detector]
-    one_D = one_D.sort_values(by=['measurement_end_utc'])
-    return one_D.tail(n=24*past_days)['n_vehicles_in_interval'].mean()
+    one_D = one_D.sort_values(by=["measurement_end_utc"])
+    return one_D.tail(n=24 * past_days)["n_vehicles_in_interval"].mean()
 
-def MALD(df: pd.DataFrame, detector: str, past_days: int, hour: int)-> float:
+
+def MALD(df: pd.DataFrame, detector: str, past_days: int, hour: int) -> float:
 
     """Proportion of counts at this hour, looking back at all historical data
 
@@ -41,12 +43,22 @@ def MALD(df: pd.DataFrame, detector: str, past_days: int, hour: int)-> float:
 
         """
 
-    one_D = df[df["detector_id"] == detector].tail(n=24*past_days)
-    beta = (one_D[one_D['measurement_end_utc'].dt.hour == hour]['n_vehicles_in_interval'].sum())/(one_D['n_vehicles_in_interval'].sum())
+    one_D = df[df["detector_id"] == detector].tail(n=24 * past_days)
+    beta = (
+        one_D[one_D["measurement_end_utc"].dt.hour == hour][
+            "n_vehicles_in_interval"
+        ].sum()
+    ) / (one_D["n_vehicles_in_interval"].sum())
     return beta
 
-def MALDforecast(df: pd.DataFrame, detectors: list, days_in_past: int,
-                 days_in_future: int, display: bool = False)-> pd.DataFrame:
+
+def MALDforecast(
+    df: pd.DataFrame,
+    days_in_past: int,
+    days_in_future: int,
+    display: bool = False,
+    detectors: list = None,
+) -> pd.DataFrame:
 
     """Average forcast using MALD hourly proportions to account of hourly variation
 
@@ -61,6 +73,8 @@ def MALDforecast(df: pd.DataFrame, detectors: list, days_in_past: int,
         Dataframe forecast in same format as SCOOT input dataframe
 
         """
+    if detectors is None:
+        detectors = df["detector_id"].drop_duplicates().to_numpy()
 
     framelist = []
     for detector in detectors:
@@ -68,30 +82,46 @@ def MALDforecast(df: pd.DataFrame, detectors: list, days_in_past: int,
         pred = []
         endtime = []
         starttime = []
-        for i in range(1, 24*days_in_future +1):
-            end = df['measurement_end_utc'].to_numpy()[-1] + np.timedelta64(i, 'h')
-            hour = (df['measurement_end_utc'].dt.hour.to_numpy()[-1] + i)% 24
-            start = df['measurement_start_utc'].to_numpy()[-1] + np.timedelta64(i, 'h')
+        for i in range(1, 24 * days_in_future + 1):
+            end = df["measurement_end_utc"].to_numpy()[-1] + np.timedelta64(i, "h")
+            hour = (df["measurement_end_utc"].dt.hour.to_numpy()[-1] + i) % 24
+            start = df["measurement_start_utc"].to_numpy()[-1] + np.timedelta64(i, "h")
             beta = MALD(df, detector, days_in_past, hour)
             endtime.append(end)
             starttime.append(start)
-            pred.append(beta*24*meanV)
+            pred.append(beta * 24 * meanV)
 
-        df2 = pd.DataFrame({"detector_id" : detector, "lon" : df[df["detector_id"] == detector]["lon"].iloc[0], "lat" : df[df["detector_id"] == detector]["lat"].iloc[0], 'measurement_start_utc': starttime, 'measurement_end_utc':endtime, "n_vehicles_in_interval": pred})
+        df2 = pd.DataFrame(
+            {
+                "detector_id": detector,
+                "lon": df[df["detector_id"] == detector]["lon"].iloc[0],
+                "lat": df[df["detector_id"] == detector]["lat"].iloc[0],
+                "measurement_start_utc": starttime,
+                "measurement_end_utc": endtime,
+                "n_vehicles_in_interval": pred,
+            }
+        )
         framelist.append(df2)
     DF = pd.concat(framelist)
 
-    if(display):
-        df_plot = DF.set_index('measurement_end_utc')
+    if display:
+        df_plot = DF.set_index("measurement_end_utc")
         for detector in detectors:
             df_plot[df_plot["detector_id"] == detector]["n_vehicles_in_interval"].plot()
 
     return DF
 
 
-def holt_winters(df: pd.DataFrame, detectors: list, days_in_past: int, 
-                days_in_future: int, alpha: float = 0.1, beta: float = 0.1, 
-                gamma: float = 0.1, display: bool = False)-> pd.DataFrame:
+def holt_winters(
+    df: pd.DataFrame,
+    days_in_past: int,
+    days_in_future: int,
+    alpha: float = 0.1,
+    beta: float = 0.1,
+    gamma: float = 0.1,
+    display: bool = False,
+    detectors: list = None,
+) -> pd.DataFrame:
 
     """Average forecast using Holt-Winters method
 
@@ -108,51 +138,70 @@ def holt_winters(df: pd.DataFrame, detectors: list, days_in_past: int,
 
         """
 
-    framelist=[]
+    if detectors is None:
+        detectors = df["detector_id"].drop_duplicates().to_numpy()
+
+    framelist = []
     for detector in detectors:
-        S=1
-        T=1
-        I=np.ones(24)
-        one_D=df[df["detector_id"]==detector]
-        one_D=one_D.sort_values(by=['measurement_end_utc'])
-        past=one_D.tail(n=24*days_in_past)
+        S = 1
+        T = 1
+        I = np.ones(24)
+        one_D = df[df["detector_id"] == detector]
+        one_D = one_D.sort_values(by=["measurement_end_utc"])
+        past = one_D.tail(n=24 * days_in_past)
         for i in range(0, len(past)):
-            h=i%24
+            h = i % 24
             c = past["n_vehicles_in_interval"].iloc[i]
-            Snew = (alpha*(c/I[h])) + (1-alpha)*(S + T)
-            T = beta*(Snew - S) + (1-beta)*T
-            I[h] = gamma*(c/Snew) + (1-gamma)*I[h]
-            S=Snew
+            Snew = (alpha * (c / I[h])) + (1 - alpha) * (S + T)
+            T = beta * (Snew - S) + (1 - beta) * T
+            I[h] = gamma * (c / Snew) + (1 - gamma) * I[h]
+            S = Snew
 
         baseline = []
         endtime = []
         starttime = []
-        for j in range(0, days_in_future*24):
-            end=df['measurement_end_utc'].to_numpy()[-1] + np.timedelta64(j, 'h')
-            start=df['measurement_start_utc'].to_numpy()[-1] + np.timedelta64(j, 'h')
-            h=j%24
-            b=(S + T)*I[h]
+        for j in range(0, days_in_future * 24):
+            end = df["measurement_end_utc"].to_numpy()[-1] + np.timedelta64(j, "h")
+            start = df["measurement_start_utc"].to_numpy()[-1] + np.timedelta64(j, "h")
+            h = j % 24
+            b = (S + T) * I[h]
             baseline.append(b)
             endtime.append(end)
             starttime.append(start)
 
-            Snew = (alpha*(b/I[h])) + (1-alpha)*(S + T)
-            T = beta*(Snew - S) + (1-beta)*T
-            I[h] = gamma*(b/Snew) + (1-gamma)*I[h]
-            S=Snew
+            Snew = (alpha * (b / I[h])) + (1 - alpha) * (S + T)
+            T = beta * (Snew - S) + (1 - beta) * T
+            I[h] = gamma * (b / Snew) + (1 - gamma) * I[h]
+            S = Snew
 
-        df2 = pd.DataFrame({"detector_id" : detector, "lon" : df[df["detector_id"]==detector]["lon"].iloc[0], "lat" : df[df["detector_id"]==detector]["lat"].iloc[0], 'measurement_start_utc': starttime, 'measurement_end_utc':endtime, "n_vehicles_in_interval": baseline})
+        df2 = pd.DataFrame(
+            {
+                "detector_id": detector,
+                "lon": df[df["detector_id"] == detector]["lon"].iloc[0],
+                "lat": df[df["detector_id"] == detector]["lat"].iloc[0],
+                "measurement_start_utc": starttime,
+                "measurement_end_utc": endtime,
+                "n_vehicles_in_interval": baseline,
+            }
+        )
         framelist.append(df2)
-    DF=pd.concat(framelist)
-    
-    if(display):
-        df_plot=DF.set_index('measurement_end_utc')
+    DF = pd.concat(framelist)
+
+    if display:
+        df_plot = DF.set_index("measurement_end_utc")
         for detector in detectors:
-            df_plot[df_plot["detector_id"]==detector]["n_vehicles_in_interval"].plot()
+            df_plot[df_plot["detector_id"] == detector]["n_vehicles_in_interval"].plot()
 
     return DF
 
-def count_baseline(df: pd.DataFrame, detectors: list, days_in_past: int, days_in_future: int, method: str ="HW"):
+
+def count_baseline(
+    df: pd.DataFrame,
+    days_in_past: int,
+    days_in_future: int,
+    method: str = "HW",
+    detectors: list = None,
+):
 
     """Produces a DataFrame where the count and baseline can be compared for use
         in scan statistics
@@ -169,24 +218,54 @@ def count_baseline(df: pd.DataFrame, detectors: list, days_in_past: int, days_in
 
         """
 
-    prediction_start = df["measurement_end_utc"].iloc[-1] - np.timedelta64(days_in_future*24, 'h')
+    if detectors is None:
+        detectors = df["detector_id"].drop_duplicates().to_numpy()
 
-    train_data=df[df["measurement_end_utc"]<=prediction_start]
-    test_data=df[df["measurement_end_utc"]>prediction_start]
+    prediction_start = df["measurement_end_utc"].iloc[-1] - np.timedelta64(
+        days_in_future * 24, "h"
+    )
 
-    if(method=="HW"):
-        y=holt_winters(train_data, detectors, days_in_past, days_in_future, alpha=0.05, beta=0.05, gamma=0.2)
-    if(method=="MALD"):
-        y=MALDforecast(train_data, detectors, days_in_past, days_in_future)
-    sd=[]
+    train_data = df[df["measurement_end_utc"] <= prediction_start]
+    test_data = df[df["measurement_end_utc"] > prediction_start]
+
+    if method == "HW":
+        y = holt_winters(
+            train_data,
+            days_in_past,
+            days_in_future,
+            alpha=0.06,
+            beta=0.02,
+            gamma=0.6,
+            detectors=detectors,
+        )
+    if method == "MALD":
+        y = MALDforecast(
+            train_data, detectors, days_in_past, days_in_future, detectors=detectors
+        )
+    sd = []
 
     for detector in detectors:
 
-        sd.append(test_data[test_data['detector_id']==detector])
+        sd.append(test_data[test_data["detector_id"] == detector])
 
-    sample_test_data=pd.concat(sd)
+    sample_test_data = pd.concat(sd)
 
-    Y=y.merge(sample_test_data, on=["lon", "lat","measurement_end_utc", "detector_id", "measurement_start_utc"], how='left')
-    Y=Y.rename(columns={"n_vehicles_in_interval_x": "baseline", "n_vehicles_in_interval_y": "count"})
+    Y = y.merge(
+        sample_test_data,
+        on=[
+            "lon",
+            "lat",
+            "measurement_end_utc",
+            "detector_id",
+            "measurement_start_utc",
+        ],
+        how="left",
+    )
+    Y = Y.rename(
+        columns={
+            "n_vehicles_in_interval_x": "baseline",
+            "n_vehicles_in_interval_y": "count",
+        }
+    )
 
     return Y
