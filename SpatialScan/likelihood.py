aa8ef0ac1bf35438ba5ce8ab86f5c7b49a114cba
chancehaycock <chancehaycock@me.com>
2020-07-02 09:33:05 +0100

    Added likelihood doc

--- SpatialScan/likelihood.py
+++ SpatialScan/likelihood.py
@@ -22,6 +22,20 @@ def likelihood_ratio(B: float, C: float) -> float:
 
 
 def likelihood_ratio_kulgen(B: float, C: float, B_tot: float, C_tot: float, eps: float):
+    """Generalised likelihood expression as explained by D. Neill in more
+    recent work. Allows for tuning with parameter eps. The eps = 0 case defaults
+    to the original test statistic suggested by Kulldorf. Note that the original
+    Kulldorf statistic and this one calculate metrics based on C, B inside AND
+    outside the test region S. `likelihood_ratio()` does not; it only focuses
+    on counts inside the region of interest.
+    Args:
+        B: Sum of baseline counts in region of interest
+        C: Sum of actual counts in region of interest
+        B_tot: Sum of baseline counts in global region
+        C_tot: Sum of baseline counts in global region
+    Returns:
+        Generalised Test Statistic D_epsilon (S)
+    """
 
     # First Calculate the Sign
     condition = C / B > (1 + eps) * (C_tot - C) / (B_tot - B)
