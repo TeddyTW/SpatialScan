6720fcd9c94dfebd22005efc1bc600d13c5f4dfa
chancehaycock <chancehaycock@me.com>
2020-07-10 22:20:32 +0100

    Added BBayes Likelihood Metric

--- SpatialScan/likelihood.py
+++ SpatialScan/likelihood.py
@@ -5,6 +5,7 @@ expected, randomisation testing is used to infer the statistical signifiance
 of the event"""
 
 import numpy as np
+from scipy.special import gamma
 
 
 def likelihood_ratio(B: float, C: float) -> float:
@@ -53,3 +54,50 @@ def likelihood_ratio_kulgen(B: float, C: float, B_tot: float, C_tot: float, eps:
         + (C_tot - C) * np.log((C_tot - C) / (B_tot - B))
         - C_tot * np.log(C_tot / (B_tot + eps * B))
     )
+
+
+def bayes_lhood_form(alpha, beta, B, C):
+    if B == 0 or C == 0:
+        return np.nan
+    return (beta ** alpha) * gamma(alpha + C) / (((beta + B)**(alpha + C)) * gamma(alpha))
+
+def bbayes_lhood_H1(B_in, C_in, B_tot, C_tot, num_m_samples=10):
+
+    # Get counts outside a region
+    B_out = B_tot - B_in
+    C_out = C_tot - C_in
+
+    # Assumes baselines are estimating equality of counts. (Not proprtionality)
+    q_0 = 1
+
+    # Set most priors here
+    alpha_out = q_0 * B_out
+    beta_in = B_in
+    beta_out = B_out
+
+    av_score = 0
+    for _ in range(num_m_samples):
+
+        # Generate m value between 1 and 3 with increment 0.2
+        m = np.random.randint(5, 15) / 5
+
+        # Set the last prior which depends on m
+        alpha_in = m * q_0 * B_in
+
+        l_score = bayes_lhood_form(alpha_in, beta_in, B_in, C_in) *\
+                  bayes_lhood_form(alpha_out, beta_out, B_out, C_out)
+
+        av_score += (l_score / num_m_samples)
+
+    return av_score
+
+
+def bbayes_lhood_H0(B_all, C_all):
+
+    q_0 = 1
+
+    # Setting blind priors here
+    alpha_all = q_0 * B_all
+    beta_all = B_all
+
+    return bayes_lhood_form(alpha_all, beta_all, B_all, C_all)
